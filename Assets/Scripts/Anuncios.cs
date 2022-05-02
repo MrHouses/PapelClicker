269632e3c680bd1487b5bfac46e866abbc0e06c7
MrHouses <luis_casas_r@hotmail.com>
2022-05-02 12:16:55 -0500

    Update Anuncios.cs

--- Assets/Scripts/Anuncios.cs
+++ Assets/Scripts/Anuncios.cs
@@ -14,12 +14,7 @@ public class Anuncios : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       // string adUnitId = "ca-app-pub-3940256099942544/5224354917";//"ca-app-pub-4609727598306757/8106350144";
-       // _bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Center);
-
-        //AdRequest request = new AdRequest.Builder().AddTestDevice("C5FFE9677FA0E0EBAA6D10D89F35E1A1").Build();
-        //_bannerView.LoadAd(request);            
-        
+       
        
         MobileAds.Initialize(appUnitId);
         this.ad = RewardBasedVideoAd.Instance;
